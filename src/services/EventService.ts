90140ed5010cca0891ea0023e6f04f453f669cd6
Liu Jiahao <3221295761@qq.com>
2025-10-24 12:35:42 +0800

    Third Commit

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/LJH-z1/vue-mock-data/events',
+  baseURL: 'https://my-json-server.typicode.com/LJH-z1/vue-mock-data',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
@@ -10,7 +10,10 @@ const apiClient = axios.create({
 })
 
 export default {
- getEvents() {
-    return apiClient.get('/events')
+ getEvents(perPage: Number, page: Number) {
+    return apiClient.get(`/events?_limit=${perPage}&_page=${page}`)
+  },
+  getEvent(id: number) {
+    return apiClient.get('/events/' + id)
   }
 }
